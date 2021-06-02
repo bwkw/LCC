1bd730ae6db8fb094410367b08ddc09c75b656ca
S.Naito <na.shota.1139@gmail.com>
2021-06-02 14:48:21 +0900

    レスポンシブ対応途中

---
+++ home/home.php
@@ -0,0 +1,152 @@
+<?php
+session_start();
+$_SESSION["URL"] = $_SERVER['REQUEST_URI'];
+
+if ($_SESSION["computer"]  === null || $_SESSION["computer"]  === "") {
+  $_SESSION["URL"] = $_SERVER['REQUEST_URI'];
+  header("Location: http://co-19-216.99sv-coco.com/lcc/user_judge.php");
+  exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> 
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="home.css">
+
+  <?php if ($_SESSION["computer"] === "pc") { ?>
+    <link rel="stylesheet" href="home_pc.css">
+    <?php } elseif ($_SESSION["computer"] === "phone") { ?>
+      <link rel="stylesheet" href="home_phone.css">
+  <?php } ?>
+  
+  <title>HOME</title>
+</head>
+<body>
+  <!-- header用div -->
+  <?php if ($_SESSION["computer"] === "pc") { ?>
+    <header> 
+      <div class="header_div">
+        <div class="div_ul">
+          <ul class="header_ul">
+            <li class="logo_url">
+              <div class="logo_url_div">
+                <a  href="http://co-19-216.99sv-coco.com/lcc/home/home.php" class="logo_a">
+                  <img class="img_lcc"src="../../img/4040.png">
+                  <p class="header_text">LCC</p>
+                </a>
+              </div>
+            </li>
+            <li class=""><a  href="http://co-19-216.99sv-coco.com/lcc/service/home.php" class=""><p class="header_text">Service</p></a></li>
+            <li class=""><a  href="" class=""><p class="header_text">Blog</p></a></li>
+            <li class=""><a  href="" class=""><p class="header_text">Summary</p></a></li>
+            <li class=""><a  href="" class=""><p class="header_text">SNS</p></a></li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  <?php } elseif ($_SESSION["computer"] === "phone") { ?>
+    <div class="phone_header">
+      <div class="hamburger">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+  
+      <nav class="globalMenuSp">
+        <ul>
+            <li><a href="http://co-19-216.99sv-coco.com/lcc/home/home.php">Home</a></li>
+            <li><a href="http://co-19-216.99sv-coco.com/lcc/service/home.php">Service</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Summary</a></li>
+            <li><a href="#">SNS</a></li>
+        </ul>
+      </nav>
+  
+      <header> 
+        <div class="header_div">
+          <div class="div_ul">
+            <ul class="header_ul">
+              <li class="logo_url">
+                <div class="logo_url_div">
+                  <a  href="" class="logo_a">
+                    <img class="img_lcc" src="../img/4040.png">
+                    <p class="header_text">LCC</p>
+                  </a>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </header>
+    </div>
+  <?php } ?>
+
+  <div class="description_div">
+        <div class="lcc">LCC</div>
+        <div class="sentence effect-fade">あなたに「夢」はありますか？</div>
+  </div>
+
+<!-- footer用div -->
+<div class="footer_div"> 
+    <div class="Instagram_div">
+      <div class="Instagram_icon_div">
+        <a target="_blank" class="Instagram_icon_a" href="https://www.instagram.com/lcc_it/">
+          <img class="Instagram_icon" src="https://img.icons8.com/nolan/64/instagram-new.png"/>
+        </a>
+        <p class="Instagram_icon_p">Instagram [@LCC_IT]</p>
+      </div>
+    </div>
+    <p class="footer_p">©︎ Life Can Change 2021</p>
+  </div>
+
+  <script>
+    $(function(){
+      $(window).scroll(function (){
+        $('.effect-fade').each(function(){
+            var elemPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > elemPos - windowHeight){
+                $(this).addClass('effect-scroll');
+            }
+        });
+      });
+    });
+    window.onload = function() {
+      scroll_effect();
+    
+      $(window).scroll(function(){
+        scroll_effect();
+      });
+      function scroll_effect(){
+        $('.effect-fade').each(function(){
+        var elemPos = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll > elemPos - windowHeight){
+          $(this).addClass('effect-scroll');
+        }
+        });
+      }
+    };
+
+    $(function() {
+      $('.hamburger').click(function() {
+          $(this).toggleClass('active');
+    
+          if ($(this).hasClass('active')) {
+              $('.globalMenuSp').addClass('active');
+          } else {
+              $('.globalMenuSp').removeClass('active');
+          }
+      });
+    });
+
+  </script>
+
+</body>
+</html>
